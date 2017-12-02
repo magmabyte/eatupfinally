96e0402d7f408d24925662754762e328ccbfe19b
Maximilian Brehm <maximilian.brehm@student.hpi.de>
2017-12-02 20:07:24 +0100

    push visualizer api

--- Assets/MarioVisualizer.cs
+++ Assets/MarioVisualizer.cs
@@ -4,13 +4,55 @@ using UnityEngine;
 
 public class MarioVisualizer : MonoBehaviour {
 
-	// Use this for initialization
+    public void SetSlim(bool isSlim)
+    {
+        // get state
+        _animator = isSlim ? SlimAnimator : FatAnimmator;
+        // set the same state
+    }
+
+    private Animator _animator;
+    public Animator SlimAnimator;
+    public Animator FatAnimmator;
+
 	void Start () {
-		
+        _animator = SlimAnimator;
+
 	}
 	
-	// Update is called once per frame
+    public bool DebugSwitchSlim = false;
+    public bool DebugIsSlim = true;
 	void Update () {
-		
+		if (DebugSwitchSlim)
+        {
+            DebugIsSlim = !DebugIsSlim;
+            SetSlim(DebugIsSlim);
+            DebugSwitchSlim = false;
+        }
 	}
+    
+    public void Walk()
+    {
+        _animator.SetTrigger("Walk");
+    }
+
+    public void WalkBackwards()
+    {
+        _animator.SetTrigger("WalkBackwards");
+    }
+
+    public void Jump()
+    {
+
+    }
+
+    public void JumpBackwards()
+    {
+
+    }
+
+    public void Idle()
+    {
+
+    }
 }
