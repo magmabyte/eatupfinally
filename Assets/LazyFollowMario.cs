f2e40c2300e6e9cb152b6f304f213275dd090cab
Maximilian Brehm <maximilian.brehm@student.hpi.de>
2017-12-02 17:47:57 +0100

    Initial project files

---
+++ Assets/LazyFollowMario.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LazyFollowMario : MonoBehaviour {
+    public float DampTime = 0.15f;
+    private Vector3 _velocity = Vector3.zero;
+    public Transform Target;
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    void LateUpdate()
+    {
+        if (Target)
+        {
+            Vector3 point = _camera.WorldToViewportPoint(Target.position);
+            Vector3 delta = Target.position - _camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref _velocity, DampTime);
+        }
+
+    }
+}
---
+++ Assets/MarioController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioController : MonoBehaviour {
+
+    private Rigidbody2D _rigidbody;
+    public float VelocityMultiplier = 1f;
+
+    public float JumpVelocity = 1f;
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    // Use this for initialization
+    void Start () {
+        _rigidbody = GetComponent<Rigidbody2D>();
+	}
+    // b jump, a sprint
+    // left right walk
+    // Update is called once per frame
+
+    private bool _grounded
+    {
+        get
+        {
+            return _numGrounded > 0;
+        }
+    }
+    private int _numGrounded;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            Debug.Log("Grounded");
+            _numGrounded++;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            Debug.Log("Ungrounded");
+
+            _numGrounded--;
+        }
+    }
+
+    void FixedUpdate () {
+        var h = Input.GetAxis("Horizontal");
+
+        _rigidbody.velocity += VelocityMultiplier * h * Vector2.right * Time.fixedDeltaTime;
+
+        if (Input.GetButton("Jump") && _grounded)
+        {
+            _rigidbody.velocity += Vector2.up * JumpVelocity * Time.fixedDeltaTime;
+        }
+
+        if (_rigidbody.velocity.y < -0.01)
+        {
+            _rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
+        else if (_rigidbody.velocity.y > 0.01 && !Input.GetButton("Jump"))
+        {
+            _rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.fixedDeltaTime;
+        }
+
+        //if (Input.anyKey)
+        //{
+        //    foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+        //    {
+        //        if (Input.GetKeyDown(kcode))
+        //            Debug.Log("Key pressed : " + kcode);
+        //    }
+        //}
+    }
+}
